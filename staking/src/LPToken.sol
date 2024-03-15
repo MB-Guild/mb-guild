bf07ec865d7538b7191ddd43e474c3e0c715ff92
Mr. B <125715895+0xilbiscione@users.noreply.github.com>
2024-03-15 03:50:25 +0000

    commit

---
+++ staking/src/LPToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "@thirdweb-dev/contracts/base/ERC721LazyMint.sol";
+
+contract LPToken is ERC721LazyMint {
+    constructor(
+        address _defaultAdmin,
+        string memory _name,
+        string memory _symbol,
+        address _royaltyRecipient,
+        uint128 _royaltyBps
+    ) ERC721LazyMint(_defaultAdmin, _name, _symbol, _royaltyRecipient, _royaltyBps) {}
+}
