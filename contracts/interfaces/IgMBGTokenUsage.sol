239960aa5fca60d44e526d68ac29a2e81bd5e5db
Mr. B <125715895+0xilbiscione@users.noreply.github.com>
2024-03-15 03:19:04 +0000

    sorting contract

--- contracts/interfaces/IgMBGTokenUsage.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-interface IgMBGTokenUsage {
-    function allocate(address userAddress, uint256 amount, bytes calldata data) external;
-    function deallocate(address userAddress, uint256 amount, bytes calldata data) external;
-}
